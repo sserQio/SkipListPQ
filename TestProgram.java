65504298c23e8b55a1d17bd21a7ccb5102e8fa72
sergio <papolino10@gmail.com>
2024-12-15 15:13:18 +0100

    Copiato il template

---
+++ TestProgram.java
@@ -0,0 +1,118 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+//Class my entry
+class MyEntry {
+    private Integer key;
+    private String value;
+    public MyEntry(Integer key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+    public Integer getKey() {
+        return key;
+    }
+    public String getValue() {
+        return value;
+    }
+    @Override
+    public String toString() {
+        return key + " " + value;
+    }
+}
+
+//Class SkipListPQ
+class SkipListPQ {
+
+    private double alpha;
+    private Random rand;
+
+    public SkipListPQ(double alpha) {
+        this.alpha = alpha;
+        this.rand = new Random();
+	// TO BE COMPLETED       
+    }
+
+    public int size() {
+	// TO BE COMPLETED       
+    }
+
+    public MyEntry min() {
+	// TO BE COMPLETED 
+    }
+
+    public int insert(int key, String value) {
+	// TO BE COMPLETED 
+    }
+
+    private int generateEll(double alpha_ , int key) {
+        int level = 0;
+        if (alpha_ >= 0. && alpha_< 1) {
+          while (rand.nextDouble() < alpha_) {
+              level += 1;
+          }
+        }
+        else{
+          while (key != 0 && key % 2 == 0){
+            key = key / 2;
+            level += 1;
+          }
+        }
+        return level;
+    }
+
+    public MyEntry removeMin() {
+	// TO BE COMPLETED 
+    }
+
+    public void print() {
+	// TO BE COMPLETED 
+    }
+}
+
+//TestProgram
+
+public class TestProgram {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java TestProgram <file_path>");
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            String[] firstLine = br.readLine().split(" ");
+            int N = Integer.parseInt(firstLine[0]);
+            double alpha = Double.parseDouble(firstLine[1]);
+            System.out.println(N + " " + alpha);
+
+            SkipListPQ skipList = new SkipListPQ(alpha);
+
+            for (int i = 0; i < N; i++) {
+                String[] line = br.readLine().split(" ");
+                int operation = Integer.parseInt(line[0]);
+
+                switch (operation) {
+                    case 0:
+			// TO BE COMPLETED 
+                        break;
+                    case 1:
+			// TO BE COMPLETED 
+                        break;
+                    case 2:
+			// TO BE COMPLETED 
+                        break;
+                    case 3:
+			// TO BE COMPLETED 
+                        break;
+                    default:
+                        System.out.println("Invalid operation code");
+                        return;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
